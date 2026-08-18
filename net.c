7f89a2d8ccb3582e4102eaf2b181fa5484157349
kirapika2 <ray.quasar36@gmail.com>
2026-08-19 00:06:38 +0900

    Completion of step2

--- net.c
+++ net.c
@@ -54,6 +54,14 @@ net_device_open(struct net_device *dev)
         errorf("already opened, dev=%s", dev->name);
         return -1;
     }
+    if (dev->ops->open)
+    {
+        if (dev->ops->open(dev) == -1)
+        {
+            errorf("failure, dev=%s", dev->name);
+            return -1;
+        }
+    }
     dev->flags |= NET_DEVICE_FLAG_UP; // フラグを立てる
     return 0;
 }
@@ -67,6 +75,14 @@ net_device_close(struct net_device *dev)
     {
         errorf("not opened, dev=%s", dev->name);
         return -1;
+    }
+    if (dev->ops->close)
+    {
+        if (dev->ops->close(dev) == -1)
+        {
+            errorf("failure, dev=%s", dev->name);
+            return -1;
+        }
     }
     dev->flags &= ~NET_DEVICE_FLAG_UP; // フラグを下ろす
     return 0;
@@ -87,11 +103,31 @@ int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data
         errorf("too long, dev=%s, mtu=%u, len=%zu", dev->name, dev->mtu, len);
         return -1;
     }
+    if (!dev->ops->output)
+    {
+        errorf("ouput callback function is not set, dev=%s", dev->name);
+        return -1;
+    }
+    if (dev->ops->output(dev, type, data, len, dst) == -1)
+    {
+        errorf("failure, dev=%s, len=%zu", dev->name, len);
+        return -1;
+    }
     return 0;
 }
 
+// ネットワークデバイスからのデータ入力
+/*
+type: 入力パケットのプロトコル種別
+data: 入力パケットのバイト列
+len: 入力パケットのバイト数
+dev: 入力パケットを受信したネットワークデバイスのポインタ
+*/
 int net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
+    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
+    debugdump(data, len);
+    return 0;
 }
 
 int net_init(void)
--- test/test.c
+++ test/test.c
@@ -6,36 +6,12 @@
 
 #include "util.h"
 #include "net.h"
+#include "driver/loopback.h"
 
 #include "test.h"
 
 static volatile sig_atomic_t terminate;
 static struct net_device *dev;
-
-// ダミーデバイス登録
-struct net_device *
-dummy_init(void)
-{
-    struct net_device *dev;
-
-    dev = net_device_alloc();
-    if (!dev)
-    {
-        errorf("net_device_alloc() failed");
-        return NULL;
-    }
-    dev->type = NET_DEVICE_TYPE_DUMMY;
-    dev->mtu = 128;
-    dev->hlen = 0; /* no header */
-    dev->alen = 0; /* no address */
-    if (net_device_register(dev) == -1)
-    {
-        errorf("net_device_register() failed");
-        return NULL;
-    }
-    infof("success, dev=%s", dev->name);
-    return dev;
-}
 
 // シグナルハンドラ
 static void
@@ -63,10 +39,10 @@ setup(void)
         errorf("net_init() failure");
         return -1;
     }
-    dev = dummy_init();
+    dev = loopback_init();
     if (!dev)
     {
-        errorf("dummy_init() failure");
+        errorf("loopback_init() failure");
         return -1;
     }
     if (net_run() == -1)
